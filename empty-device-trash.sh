1e5f148d729f217a26a6f98a1e5352a63101ac58
Christian Hartmann <hartmann.christian@gmail.com>
2026-03-25 12:55:15 +0100

    change sorry message

--- empty-device-trash.sh
+++ empty-device-trash.sh
@@ -5,6 +5,13 @@
 # sample usage:
 # $ empty-device-trash.sh '/dev/sdd2' '/media/christian/TOSHIBA EXT4'
 
+# TODO 0.3.0
+# use kdialog --checklist with all items preselected
+# allow deselect
+# and evaluate the returned list
+# NOTE
+# spaces in file names might cause trouble!
+
 block_device="$1"
 mount_point="$2"
 user_id=$UID
@@ -29,10 +36,10 @@ function collect_pathes
 	trash_dir="$1"
 	cd "$trash_dir"
 	for if in "${trash_dir}"/info/*.trashinfo; do
-		orig_path=$(grep '^Path=' "$if")
+		orig_path=$(grep "^Path=" "$if")
 		orig_path=${orig_path#Path=}
 		orig_path=$(decode_url "$orig_path")
- 		printf '%s\n' "$orig_path"
+ 		printf "%s\n" "$orig_path"
 	done
 }
 
@@ -48,15 +55,19 @@ mountpoint "$mount_point" 1>/dev/null 2>&1
 mount_point_check=$?
 test $mount_point_check -eq 0 || error_exit "Nothing mounted at given mount point '$mount_point'"
 
-udisksctl_out=($(command udisksctl info --block-device $block_device | grep 'IdLabel:'))
+udisksctl_out=($(command udisksctl info --block-device $block_device | grep "IdLabel:"))
 unset udisksctl_out[0]
 block_device_label="${udisksctl_out[@]}"
 
 files_count=$(cd "${mount_point}/.Trash-${user_id}/info/"; command ls -1 | command wc -l)
-sorry_message="It seems as if there are no files or directories to delete in the Trash on: ${block_device_label}${spacer}"
+#sorry_message="It seems as if there are no files or directories to delete in the Trash on: ${block_device_label}${spacer}"
+sorry_message="Can not find any files or directories to delete in the Trash on:  ${block_device_label}${spacer}"
 test $files_count -eq 0 && kdialog --title "No File(s) to Delete" --ok-label "Dismiss" --sorry "$sorry_message" 1>/dev/null 2>&1 && exit 1
 
 warning_message="Delete following $files_count file(s) permanently from trash on: ${block_device_label}?${spacer}\n\n${separator}\n$(collect_pathes "${mount_point}/.Trash-${user_id}")\n${separator}\n\nTHIS ACTION CANNOT BE UNDONE.\n\n"
 kdialog --title "Confirm Delete Permanently" --yes-label "Delete Permanently" --no-label "Cancel" --warningyesno "$warning_message" 1>/dev/null 2>&1; kdialog_return_value=$?
 
 test $kdialog_return_value -eq 0 && command rm -rf "${mount_point}/.Trash-${user_id}"/files/* && command rm -rf "${mount_point}/.Trash-${user_id}"/info/*;
+
+# check for single quotes here:
+# ''
